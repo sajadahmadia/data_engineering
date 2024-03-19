9ad2550d68353922e70f97296494d9ec87155f67
Sajad Ahmadi Jozdani <61582647+sajadahmadia@users.noreply.github.com>
2024-03-19 17:04:30 +0100

    create_table added

---
+++ Airflow_Tableau_customer_clustering/create_table.sql
@@ -0,0 +1,16 @@
+create table MOCK_DATA (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20),
+	`function` VARCHAR(50),
+	company VARCHAR(50),
+	last_bought_product VARCHAR(50),
+	isActive VARCHAR(50),
+	`status` VARCHAR(50),
+	last_login DATE,
+	clientSince VARCHAR(50),
+	`language` VARCHAR(50)
+);
